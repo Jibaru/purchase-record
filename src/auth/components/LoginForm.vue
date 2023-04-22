6949107514a79866ea618cdceb8d6724435c95f7
jibaru <ignacioruedaboada@hotmail.com>
2023-04-22 17:27:29 -0500

    feat: update auth module

---
+++ src/auth/components/LoginForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <base-card id="card">
+    <form>
+      <base-title>Registro de compras</base-title>
+      <base-label>Email</base-label>
+      <base-input type="email" required />
+      <base-label>Password</base-label>
+      <base-input type="password" required />
+      <base-button id="button">Ingresar</base-button>
+    </form>
+  </base-card>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {}
+  }
+})
+</script>
+<style lang="css" scoped>
+#card {
+  width: 300px;
+  margin: 2rem auto;
+}
+
+#button {
+  width: 100%;
+}
+</style>
---
+++ src/auth/views/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+  <login-form />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import LoginForm from '@/auth/components/LoginForm.vue'
+
+export default defineComponent({
+  components: {
+    'login-form': LoginForm
+  },
+  data() {
+    return {}
+  }
+})
+</script>
