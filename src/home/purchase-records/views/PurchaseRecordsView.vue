9f39135d26d9b5d87b50c70eba81ba82ea9cf2d6
jibaru <ignacioruedaboada@hotmail.com>
2023-05-27 16:44:24 -0500

    feat: update purchase records view

--- src/home/purchase-records/views/PurchaseRecordsView.vue
+++ src/home/purchase-records/views/PurchaseRecordsView.vue
@@ -1,13 +1,16 @@
 <template>
   <base-title class="title">Registro de compras</base-title>
+  <purchase-records-filter />
   <purchase-records-table id="table" />
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import PurchaseRecordsFilter from '../components/PurchaseRecordsFilter.vue'
 import PurchaseRecordsTable from '../components/PurchaseRecordsTable.vue'
 
 export default defineComponent({
   components: {
+    'purchase-records-filter': PurchaseRecordsFilter,
     'purchase-records-table': PurchaseRecordsTable
   },
   data() {
