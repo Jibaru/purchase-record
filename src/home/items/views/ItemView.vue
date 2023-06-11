dbe7cd2bc5b649494c19eda04ee0bfb852235d17
jibaru <ignacioruedaboada@hotmail.com>
2023-06-11 15:50:08 -0500

    feat: add item view

---
+++ src/home/items/views/ItemView.vue
@@ -0,0 +1,34 @@
+<template>
+  <base-title class="title">
+    Artículo referencia: <base-badge>{{ reference }}</base-badge>
+  </base-title>
+  <item-suppliers-table :id="reference" />
+</template>
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue'
+import ItemSuppliersTable from '../components/ItemSuppliersTable.vue'
+
+export default defineComponent({
+  props: {
+    reference: {
+      type: String as PropType<string>,
+      required: true
+    }
+  },
+  components: {
+    'item-suppliers-table': ItemSuppliersTable
+  },
+  data() {
+    return {}
+  }
+})
+</script>
+<style lang="css" scoped>
+#table {
+  margin-top: 1rem;
+}
+
+.title {
+  margin-bottom: 1rem;
+}
+</style>
