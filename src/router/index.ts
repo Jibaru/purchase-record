d4bf86a6259c6b22c38c4d090edd7e8428329f2e
jibaru <ignacioruedaboada@hotmail.com>
2023-06-11 15:51:33 -0500

    feat: add new item and supplier routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../auth/views/LoginView.vue'
 import HomeView from '../home/views/HomeView.vue'
 import PurchaseRecordsView from '../home/purchase-records/views/PurchaseRecordsView.vue'
+import SuppliersView from '../home/suppliers/views/SuppliersView.vue'
+import SupplierView from '../home/suppliers/views/SupplierView.vue'
+import ItemsView from '../home/items/views/ItemsView.vue'
+import ItemView from '../home/items/views/ItemView.vue'
 import UsersView from '../home/users/views/UsersView.vue'
 import VouchersView from '../home/vouchers/views/VouchersView.vue'
 import VoucherView from '../home/voucher/views/VoucherView.vue'
@@ -39,6 +43,28 @@ const router = createRouter({
           path: 'purchase-records',
           name: 'purchaseRecords',
           component: PurchaseRecordsView
+        },
+        {
+          path: 'suppliers',
+          name: 'suppliers',
+          component: SuppliersView
+        },
+        {
+          path: '/suppliers/:reference',
+          name: 'supplier',
+          props: true,
+          component: SupplierView
+        },
+        {
+          path: 'items',
+          name: 'items',
+          component: ItemsView
+        },
+        {
+          path: '/items/:reference',
+          name: 'item',
+          props: true,
+          component: ItemView
         }
       ]
     }
