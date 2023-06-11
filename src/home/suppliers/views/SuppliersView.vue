2c200114500b78aa16f932bc4ace6a7df821fc85
jibaru <ignacioruedaboada@hotmail.com>
2023-06-11 15:51:09 -0500

    feat: add suppliers view

---
+++ src/home/suppliers/views/SuppliersView.vue
@@ -0,0 +1,29 @@
+<template>
+  <base-title class="title">Proveedores</base-title>
+  <suppliers-filter />
+  <suppliers-table id="table" />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import SuppliersFliter from '../components/SuppliersFilter.vue'
+import SuppliersTable from '../components/SuppliersTable.vue'
+
+export default defineComponent({
+  components: {
+    'suppliers-filter': SuppliersFliter,
+    'suppliers-table': SuppliersTable
+  },
+  data() {
+    return {}
+  }
+})
+</script>
+<style lang="css" scoped>
+#table {
+  margin-top: 1rem;
+}
+
+.title {
+  margin-bottom: 1rem;
+}
+</style>
