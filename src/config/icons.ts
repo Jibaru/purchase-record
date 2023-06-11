cdc43720194d66429df415c5505439e4b082e7c5
jibaru <ignacioruedaboada@hotmail.com>
2023-06-11 16:03:23 -0500

    feat: add new icons

--- src/config/icons.ts
+++ src/config/icons.ts
@@ -1,10 +1,24 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faFileInvoice, faHome } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBars,
+  faBox,
+  faFileInvoice,
+  faHome,
+  faMoneyBill,
+  faRightFromBracket,
+  faTruckField,
+  faUser
+} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 library.add(faBars)
+library.add(faBox)
 library.add(faFileInvoice)
 library.add(faHome)
+library.add(faMoneyBill)
+library.add(faRightFromBracket)
+library.add(faTruckField)
+library.add(faUser)
 
 export const addIcons = (app: any): void => {
   app.component('font-awesome-icon', FontAwesomeIcon)
