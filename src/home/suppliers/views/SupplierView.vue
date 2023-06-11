b9a8cbce89dfacbc701f14a96170539ec1422236
jibaru <ignacioruedaboada@hotmail.com>
2023-06-11 15:51:16 -0500

    feat: add supplier view

---
+++ src/home/suppliers/views/SupplierView.vue
@@ -0,0 +1,34 @@
+<template>
+  <base-title class="title">
+    Proveedor referencia: <base-badge>{{ reference }}</base-badge>
+  </base-title>
+  <supplier-items-table :id="reference" />
+</template>
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue'
+import SupplierItemsTable from '../components/SupplierItemsTable.vue'
+
+export default defineComponent({
+  props: {
+    reference: {
+      type: String as PropType<string>,
+      required: true
+    }
+  },
+  components: {
+    'supplier-items-table': SupplierItemsTable
+  },
+  data() {
+    return {}
+  }
+})
+</script>
+<style lang="css" scoped>
+#table {
+  margin-top: 1rem;
+}
+
+.title {
+  margin-bottom: 1rem;
+}
+</style>
