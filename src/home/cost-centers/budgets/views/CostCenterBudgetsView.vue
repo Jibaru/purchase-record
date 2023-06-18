93f99ed630b1254d163cfd79580dc60fe36cb4a7
jibaru <ignacioruedaboada@hotmail.com>
2023-06-18 01:06:42 -0500

    feat: add cost center budgets view

---
+++ src/home/cost-centers/budgets/views/CostCenterBudgetsView.vue
@@ -0,0 +1,35 @@
+<template>
+  <base-title class="title">Presupuestos de Centro de Costos</base-title>
+  <cost-center-budget-form :cost-center-id="costCenterId" />
+  <cost-center-budgets-table id="table" :cost-center-id="costCenterId" />
+</template>
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue'
+import CostCenterBudgetForm from '../components/CostCenterBudgetForm.vue'
+import CostCenterBudgetsTable from '../components/CostCenterBudgetsTable.vue'
+
+export default defineComponent({
+  components: {
+    'cost-center-budget-form': CostCenterBudgetForm,
+    'cost-center-budgets-table': CostCenterBudgetsTable
+  },
+  props: {
+    costCenterId: {
+      type: String as PropType<string>,
+      required: true
+    }
+  },
+  data() {
+    return {}
+  }
+})
+</script>
+<style lang="css" scoped>
+#table {
+  margin-top: 1rem;
+}
+
+.title {
+  margin-bottom: 1rem;
+}
+</style>
